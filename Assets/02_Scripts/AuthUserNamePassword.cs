119ea52c2dfaf3a0d1ea32eb6c552c0849891026
LeeJaeHyun <myleje@gmail.com>
2024-11-02 10:34:23 +0900

    .

--- Assets/02_Scripts/AuthUserNamePassword.cs
+++ Assets/02_Scripts/AuthUserNamePassword.cs
@@ -26,17 +26,37 @@ public class AuthUserNamePassword : MonoBehaviour
         });
     }
 
-    // 회원가입 로직
     /*
         회원이름 : 대소문자 구별없음, 3 ~ 20자, [. - @]
-        비밀번호 : 대소문자 구별, 대문자 1, 소문자 1, 숫자 1, 특수문자 1 포함
+        비밀번호 : 8자 이상, 대소문자 구별, 대문자 1, 소문자 1, 숫자 1, 특수문자 1 포함
     */
+
+    // 회원가입 로직
     private async Task SignUpUser(string username, string password)
     {
         try
         {
             await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
             Debug.Log("회원가입 성공");
+        }
+        catch (AuthenticationException e)
+        {
+            Debug.Log(e.Message);
+        }
+        catch (RequestFailedException e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    // 로그인 처리
+    private async Task SignInUser(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
+            var _playerId = AuthenticationService.Instance.PlayerId;
+            Debug.Log($"로그인 성공 Player ID : {_playerId}");
         }
         catch (AuthenticationException e)
         {
