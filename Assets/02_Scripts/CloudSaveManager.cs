755247b69e7377f664c4090229e07612c52d1774
LeeJaeHyun <myleje@gmail.com>
2024-11-02 11:06:42 +0900

    .

---
+++ Assets/02_Scripts/CloudSaveManager.cs
@@ -0,0 +1,21 @@
+using Unity.Services.Core;
+using UnityEngine;
+using Auth = Unity.Services.Authentication.AuthenticationService;
+
+public class CloudSaveManager : MonoBehaviour
+{
+    private async void Awake()
+    {
+        // 유니티 서비스 초기화
+        await UnityServices.InitializeAsync();
+
+        // 로그인 콜백
+        Auth.Instance.SignedIn += () =>
+        {
+
+        };
+
+        // 익명 로그인
+        await Auth.Instance.SignInAnonymouslyAsync();
+    }
+}
